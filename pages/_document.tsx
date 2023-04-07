716f871a02331456a77eeb853b220a55ea9e664c
MainAdc95 <mustafamohammedalhallaq.com>
2023-04-07 21:03:37 +0400

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,53 +1,34 @@
-import * as React from "react";
-import Document, {
-    Html,
-    Head,
-    Main,
-    NextScript,
-    DocumentContext,
-    DocumentProps,
-} from "next/document";
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentProps } from "next/document"
+import * as React from "react"
 
-type Props = Record<string, unknown> & DocumentProps;
+type Props = Record<string, unknown> & DocumentProps
 
 export default class MyDocument extends Document<Props> {
     static async getInitialProps(ctx: DocumentContext) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
     }
 
     render() {
-        const { locale } = this.props.__NEXT_DATA__;
+        const { locale } = this.props.__NEXT_DATA__
         return (
             <Html lang={locale}>
                 <Head>
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="180x180"
-                        href="/apple-touch-icon.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="32x32"
-                        href="/favicon-32x32.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="16x16"
-                        href="/favicon-16x16.png"
-                    />
+                    <title>Crown Phoenix Marketing Consultancy (CPMC)</title>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                     <link rel="manifest" href="/site.webmanifest" />
-                    <meta name="msapplication-TileColor" content="#da532c" />
-                    <meta name="theme-color" content="#ffffff" />
+                    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f03a3a" />
+                    <meta name="msapplication-TileColor" content="#ffffff" />
+                    <meta name="theme-color" content="#ffffff"></meta>
                 </Head>
 
-                <body className="dark:bg-darkgray-50 desktop-transparent bg-gray-100 antialiased">
+                <body className="desktop-transparent bg-gray-100 antialiased dark:bg-darkgray-50">
                     <Main />
                     <NextScript />
                 </body>
             </Html>
-        );
+        )
     }
 }
